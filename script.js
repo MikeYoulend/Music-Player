9adcb568617cecc59d90e875f4fd53b053bb009a
MikeYoulend <mikeyoulend@gmail.com>
2024-01-19 11:29:43 +0100

    Music Player

--- script.js
+++ script.js
@@ -7,4 +7,14 @@ song.onloadedmetadata = function () {
 	progress.value = song.currentTime;
 };
 
-function playPause() {}
+function playPause() {
+	if (ctrlIcon.classList.contains("fa-pause")) {
+		song.pause();
+		ctrlIcon.classList.remove("fa-pause");
+		ctrlIcon.classList.add("fa-play");
+	} else {
+		song.play();
+		ctrlIcon.classList.add("fa-pause");
+		ctrlIcon.classList.remove("fa-play");
+	}
+}
